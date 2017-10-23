54ceaadfd3e585817f0e8b2f8a6094dd8a118fa7
adi <adi@home>
2017-10-23 18:44:17 +0300

    messages scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom(){
+	// selectors
+	var messages = $("#messages");
+	var newMessage = messages.children("li:last-child");
+
+	// heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on("connect", function() {
     console.log("connected to sv");
 });
@@ -22,6 +39,7 @@ socket.on("newMessage", function(message) {
 		msg: message.msg
 	});
 	$("#messages").append(html);
+	scrollToBottom();
 
 })
 
@@ -46,6 +64,7 @@ socket.on("newLocationMessage", function(message){
 		url: message.url
 	});
 	$("#messages").append(html);
+	scrollToBottom();
 
 })
 
